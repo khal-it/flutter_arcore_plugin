4ece8a18dc91a5c8dd558a4842d743687b38097f
Khalit Machmoud Hartmann <khalit.hartmann@campus.tu-berlin.de>
2019-03-31 10:33:25 +0200

    arcore optional

--- arcore_plugin/CHANGELOG.md
+++ arcore_plugin/CHANGELOG.md
@@ -12,3 +12,7 @@
 * Added example on how to save the imgdb file in the App --> see main function in example 
 * Added pretrained imgdb file and tester images as asset for example 
 * updated README.md
+
+## 0.0.1+4
+
+* ArCore is now optional not required.
--- arcore_plugin/example/lib/main.dart
+++ arcore_plugin/example/lib/main.dart
@@ -2,8 +2,13 @@ import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:arcore_plugin/arcore_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   final Directory systemTempDir = Directory.systemTemp;
   final File tempFile = File('${systemTempDir.path}/image_database.imgdb');
 
