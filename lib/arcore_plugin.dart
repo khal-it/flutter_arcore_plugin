e4b317882671720cc26dc2fc47b39ccfcde8639a
Khalit  Hartmann <khalithartmann@Khalits-MacBook-Pro.local>
2020-07-15 18:14:07 +0200

    refactored code. added function to load file from asset into cache to the api. updated readme

--- lib/arcore_plugin.dart
+++ lib/arcore_plugin.dart
@@ -1,17 +1,14 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-typedef void ArCoreViewCreatedCallback(ArCoreViewController controller);
+typedef ArCoreViewCreatedCallback = void Function(
+    ArCoreViewController controller);
 
 class ArCoreView extends StatefulWidget {
-  final double width;
-  final double height;
-  final Function onImageRecognized;
-  final ArCoreViewCreatedCallback onArCoreViewCreated;
-  final Widget focusBox;
-
   const ArCoreView(
       {Key key,
       this.onArCoreViewCreated,
@@ -20,6 +17,12 @@ class ArCoreView extends StatefulWidget {
       this.height = 300,
       this.focusBox})
       : super(key: key);
+
+  final double width;
+  final double height;
+  final Function onImageRecognized;
+  final ArCoreViewCreatedCallback onArCoreViewCreated;
+  final Widget focusBox;
 
   @override
   _ArCoreViewState createState() => _ArCoreViewState();
@@ -45,7 +48,7 @@ class _ArCoreViewState extends State<ArCoreView> with WidgetsBindingObserver {
                 viewType: 'plugins.peqas.com/arcore_plugin',
                 onPlatformViewCreated: _onPlatformViewCreated,
               ),
-              widget.focusBox != null ? widget.focusBox : Container()
+              if (widget.focusBox != null) widget.focusBox else Container()
             ],
           ));
     }
@@ -58,7 +61,7 @@ class _ArCoreViewState extends State<ArCoreView> with WidgetsBindingObserver {
       return;
     }
     widget.onArCoreViewCreated(
-        new ArCoreViewController(id, widget.onImageRecognized));
+        ArCoreViewController(id, widget.onImageRecognized));
   }
 
   @override
@@ -69,34 +72,66 @@ class _ArCoreViewState extends State<ArCoreView> with WidgetsBindingObserver {
 }
 
 class ArCoreViewController {
-  final Function onImageRecognized;
   ArCoreViewController(int id, this.onImageRecognized) {
-    _channel = new MethodChannel('plugins.peqas.com/arcore_plugin_$id');
+    _channel = MethodChannel('plugins.peqas.com/arcore_plugin_$id');
     _channel.setMethodCallHandler(_handleMethodCalls);
   }
+  final Function onImageRecognized;
+
   MethodChannel _channel;
 
   Future<dynamic> _handleMethodCalls(MethodCall methodCall) async {
     switch (methodCall.method) {
-      case "image_recognized":
-        print(methodCall.arguments);
-        String recognizedImgName = methodCall.arguments.toString();
-        this.onImageRecognized(recognizedImgName);
-        return Future.value(recognizedImgName);
+      case 'image_recognized':
+        debugPrint(methodCall.arguments.toString());
+        final String recognizedImgName = methodCall.arguments.toString();
+        onImageRecognized(recognizedImgName);
+        return Future<String>.value(recognizedImgName);
     }
   }
 
   Future<void> resumeImageRecognition() async {
-    print("resuming image recognition");
+    debugPrint('resuming image recognition');
     return _channel.invokeMethod('resume_image_recognition');
   }
 
   Future<void> pauseImageRecognition() async {
-    print("pausing image recongition");
+    debugPrint('pausing image recongition');
     return _channel.invokeMethod('pauseImageRecognition');
   }
 
   Future<void> getArCoreView() async {
     return _channel.invokeMethod('recognize_images');
+  }
+
+  /// example of [tempFilePath] : /data/user/0/<applicationId>/cache/<name_of_imgdb.imgdb>
+  Future<void> loadImgdbFromAssets(
+      {@required String tempFilePath,
+      String imgdbAssetPath = 'assets/image_database.imgdb'}) async {
+    final File tempFile = File(tempFilePath);
+
+    // create tempfile
+    await tempFile.create();
+
+    final ByteData loadedImgdbByteData = await rootBundle.load(imgdbAssetPath);
+
+    try {
+      tempFile.writeAsBytesSync(loadedImgdbByteData.buffer.asUint8List(
+          loadedImgdbByteData.offsetInBytes,
+          loadedImgdbByteData.lengthInBytes));
+    } on Exception catch (e) {
+      throw ArCorePluginAssetLoadingException(e.toString());
+    }
+    return;
+  }
+}
+
+class ArCorePluginAssetLoadingException implements Exception {
+  ArCorePluginAssetLoadingException(this.message);
+  final String message;
+
+  @override
+  String toString() {
+    return 'ArCorePluginAssetLoadingException {message: $message}';
   }
 }
